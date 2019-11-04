8695b084a1f97fcfc4732fc8438dd7ff158fd530
trungnt-0414 <nguyen.tien.trung@sun-asterisk.com>
2019-11-04 12:46:17 +0700

    Admin panel

---
+++ engines/core/app/controllers/samurai/admin/admin_controller.rb
@@ -0,0 +1,8 @@
+module Samurai
+  module Admin
+    class AdminController < ApplicationController
+      def index
+      end
+    end
+  end
+end
--- engines/core/config/routes.rb
+++ engines/core/config/routes.rb
@@ -1,4 +1,8 @@
 Samurai::Core::Engine.routes.draw do
   devise_for :users, class_name: "Samurai::User", module: :devise
-  root to: "/samurai/dashboard#index"
+  namespace :admin do
+    resources :admin, only: :index
+    resources :users, only: :index
+  end
+  root to: "dashboard#index"
 end
