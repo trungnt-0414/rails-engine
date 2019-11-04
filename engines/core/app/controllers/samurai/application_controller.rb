e63a1bdcb7982075cb381cfc5cf111403fa5f193
trungnt-0414 <nguyen.tien.trung@sun-asterisk.com>
2019-11-04 16:28:23 +0700

    Extending core abilities

--- engines/core/app/controllers/samurai/application_controller.rb
+++ engines/core/app/controllers/samurai/application_controller.rb
@@ -4,7 +4,7 @@ module Samurai
     before_action :authenticate_user!
 
     rescue_from CanCan::AccessDenied do |exception|
-      render file: "static/403.html", status: 403, layout: false
+      render template: "static/403", status: 403, layout: false
     end
 
     private
--- engines/core/app/models/samurai/ability.rb
+++ engines/core/app/models/samurai/ability.rb
@@ -4,11 +4,33 @@ module Samurai
   class Ability
     include CanCan::Ability
 
+    class_attribute :abilities
+    self.abilities = Set.new
+
+    class << self
+      abilities = Set.new
+
+      def register_ability ability
+        abilities.add(ability)
+      end
+
+      def remove_ability ability
+        abilities.delete(ability)
+      end
+    end
+
     def initialize user
+      Rails.logger.info self.abilities.inspect
+
       if user.admin?
         can :manage, :all
       else
         can :read, :dashboard
+      end
+
+      Ability.abilities.each do |klass|
+        ability = klass.send(:new, user)
+        @rules = rules + ability.send(:rules)
       end
     end
   end
